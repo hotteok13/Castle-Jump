2232da6fd8ae400d369d6c3ac33a3c8d7f3f3261
Kileab <82032086+Unity2033@users.noreply.github.com>
2022-08-17 16:48:59 +0900

    Randomly spawn objects in a circle
    - Create an object from a circle shape
      by calculating the X and Z axes using
      equations

---
+++ Assets/Script/Follow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    private GameObject character;
+
+    void Start()
+    {
+        character = GameObject.Find("GameObject");
+    }
+
+    // Update is called once per frame
+   // void Update()
+   // {
+   //     transform.position = Vector3.MoveTowards(transform.position,
+    //        character.transform.position, 
+   //         5 * Time.deltaTime);
+   // }
+}
